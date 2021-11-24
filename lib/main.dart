1571ed96bdd4a0d8a149f2b56d9ceea6ebe2ddb8
yashthummar88 <yashthummar88@gmail.com>
2021-11-24 21:53:07 +0530

    ok google

--- lib/main.dart
+++ lib/main.dart
@@ -50,8 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+       
         child: Column(
         
           mainAxisAlignment: MainAxisAlignment.center,

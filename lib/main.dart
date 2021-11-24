273dd1242950790035a131a72e1cebb523d9011e
yashthummar88 <yashthummar88@gmail.com>
2021-11-24 21:49:50 +0530

    df

--- lib/main.dart
+++ lib/main.dart
@@ -53,12 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-         
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
